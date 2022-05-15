8c0416ae7fa8ec08865fbc23881aa005d2924d1d
khj <zla1275@gamil.com>
2022-05-15 21:13:32 +0900

    init commit

---
+++ src/main/java/com/mycompany/dailyreport/DailyreportApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.dailyreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyreportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyreportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycompany/dailyreport/DailyreportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.dailyreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyreportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
