5d396f720f7bd5e5ad9bbca64e8a985dcde43c89
khj <zla1275@gamil.com>
2022-05-16 20:44:50 +0900

    Report.java
    getter, setter 추가

--- src/main/java/com/mycompany/dailyreport/domain/Report.java
+++ src/main/java/com/mycompany/dailyreport/domain/Report.java
@@ -1,10 +1,15 @@
 package com.mycompany.dailyreport.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
+@Getter
+@Setter
 public class Report {
 
     @Id
