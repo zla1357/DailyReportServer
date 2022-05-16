27740a4c54d09d0c1b59f3c140a1e4b2e585bf5c
khj <zla1275@gamil.com>
2022-05-16 23:39:15 +0900

    DailyreportApplicationTests.java
    테스트 수정

--- src/test/java/com/mycompany/dailyreport/DailyreportApplicationTests.java
+++ src/test/java/com/mycompany/dailyreport/DailyreportApplicationTests.java
@@ -1,7 +1,13 @@
 package com.mycompany.dailyreport;
 
+import com.mycompany.dailyreport.repository.ReportRepository;
+import com.mycompany.dailyreport.service.ReportService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
 
 @SpringBootTest
 class DailyreportApplicationTests {
