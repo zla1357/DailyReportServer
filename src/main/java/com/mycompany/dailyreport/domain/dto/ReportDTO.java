d86e2b30835b3b4d1d847d48d72da5e7dba2ee42
khj <zla1275@gamil.com>
2022-05-16 22:12:58 +0900

    ReportDTO.java 추가

---
+++ src/main/java/com/mycompany/dailyreport/domain/dto/ReportDTO.java
@@ -0,0 +1,11 @@
+package com.mycompany.dailyreport.domain.dto;
+
+import lombok.Getter;
+
+@Getter
+public class ReportDTO {
+
+    private Long id;
+    private String author;
+    private String content;
+}
