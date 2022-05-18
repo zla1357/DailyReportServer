6e26d4f1dc4a2ee46098e6a5859064ffc8053540
khj <zla1275@gamil.com>
2022-05-18 23:26:01 +0900

    ReportController.java
    업무일지 등록 메서드 url 수정
    @ResponseBody 어노테이션추가
    
    업무일지 단건 조회 메서드 추가

--- src/main/java/com/mycompany/dailyreport/controller/ReportController.java
+++ src/main/java/com/mycompany/dailyreport/controller/ReportController.java
@@ -5,7 +5,11 @@ import com.mycompany.dailyreport.domain.dto.ReportDTO;
 import com.mycompany.dailyreport.service.ReportService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDateTime;
 
 @Controller
 @RequiredArgsConstructor
@@ -13,12 +17,20 @@ public class ReportController {
 
     private final ReportService reportService;
 
-    @PostMapping("report/new")
+    @PostMapping("report")
+    @ResponseBody
     public String registerReport(ReportDTO reportDTO) {
 
         Report report = new Report(reportDTO.getAuthor(), reportDTO.getContent());
         reportService.registerReport(report);
 
         return "OK";
+    }
+
+    @GetMapping("report")
+    @ResponseBody
+    public Report openReport(Long id) {
+
+        return reportService.getReport(id);
     }
 }
