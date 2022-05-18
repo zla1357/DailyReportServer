52e26af4854ae18fb81c7ada46ef7c314543a1dc
khj <zla1275@gamil.com>
2022-05-19 00:25:46 +0900

    ReportServiceTest.java
    기간으로 업무일지를 조회하는 메서드 테스트 메서드 추가

--- src/test/java/com/mycompany/dailyreport/service/ReportServiceTest.java
+++ src/test/java/com/mycompany/dailyreport/service/ReportServiceTest.java
@@ -9,6 +9,10 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -31,5 +35,28 @@ class ReportServiceTest {
         // then
         Report findReport = reportService.getReport(findId);
         assertThat(findReport).isEqualTo(report);
+    }
+
+    @Test
+    public void 기간내_업무일지_조회() throws Exception {
+        // given
+        Report report = new Report("김효중", "일했다");
+        Long saveReportId = reportService.registerReport(report);
+
+        // when
+        LocalDate endDate = LocalDate.now().plusDays(1);
+
+        String startPeriod = LocalDateTime.now().getYear() + "-" +
+                             LocalDateTime.now().getMonthValue() + "-" +
+                             LocalDateTime.now().getDayOfMonth();
+
+        String endPeriod = endDate.getYear() + "-" +
+                           endDate.getMonthValue() + "-" +
+                           endDate.getDayOfMonth();
+
+        List<Report> reports = reportService.searchReportsInPeriod(startPeriod, endPeriod);
+
+        // then
+        assertThat(reports.contains(report)).isEqualTo(true);
     }
 }
