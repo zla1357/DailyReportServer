bea31f7f1f103c78c9251a1eace4f7cf4fae20c8
khj <zla1275@gamil.com>
2022-05-19 00:23:33 +0900

    ReportRepository.java
    기간으로 업무일지를 조회하는 메서드 추가

--- src/main/java/com/mycompany/dailyreport/repository/ReportRepository.java
+++ src/main/java/com/mycompany/dailyreport/repository/ReportRepository.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
@@ -23,6 +24,13 @@ public class ReportRepository {
 
     public List<Report> findAll() {
         return em.createQuery("select r from Report", Report.class)
+                .getResultList();
+    }
+
+    public List<Report> findWithPeriod(LocalDateTime startDate, LocalDateTime endDate) {
+        return em.createQuery("select r from Report r where inputDate between :startDate and :endDate", Report.class)
+                .setParameter("startDate", startDate)
+                .setParameter("endDate", endDate)
                 .getResultList();
     }
 }
