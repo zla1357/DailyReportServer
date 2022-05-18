c36fad3e736e90c66bb5ab24450c2799178b41ff
khj <zla1275@gamil.com>
2022-05-18 23:26:16 +0900

    ReportDTO.java
    입력일, 수정일 추가

--- src/main/java/com/mycompany/dailyreport/domain/dto/ReportDTO.java
+++ src/main/java/com/mycompany/dailyreport/domain/dto/ReportDTO.java
@@ -3,10 +3,14 @@ package com.mycompany.dailyreport.domain.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
 @Getter
 @Setter
 public class ReportDTO {
 
     private String author;
     private String content;
+    private LocalDate inputDate;
+    private LocalDate updateDate;
 }
