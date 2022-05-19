a4211eab86538e60e97769ac99667f11beb7d648
khj <zla1275@gamil.com>
2022-05-20 01:20:27 +0900

    ReportServiceTest.java
    기간내_업무일지_조회
    Report 엔티티가 DTO를 받게 수정됨에 따라 생성자 호출구문 수정
    
    startPeriod, endPeriod 생성구문 수정
    
    업무일지_수정 테스트 추가

--- src/test/java/com/mycompany/dailyreport/service/ReportServiceTest.java
+++ src/test/java/com/mycompany/dailyreport/service/ReportServiceTest.java
@@ -1,7 +1,7 @@
 package com.mycompany.dailyreport.service;
 
 import com.mycompany.dailyreport.domain.Report;
-import org.assertj.core.api.Assertions;
+import com.mycompany.dailyreport.domain.dto.ReportDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +11,7 @@ import javax.persistence.EntityManager;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -27,7 +28,10 @@ class ReportServiceTest {
     @Test
     public void 업무일지_등록() throws Exception {
         // given
-        Report report = new Report("김효중", "일했다");
+        ReportDTO reportDTO = new ReportDTO();
+        reportDTO.setAuthor("김효중");
+        reportDTO.setContent("일했다");
+        Report report = new Report(reportDTO);
 
         // when
         Long findId = reportService.registerReport(report);
@@ -40,23 +44,47 @@ class ReportServiceTest {
     @Test
     public void 기간내_업무일지_조회() throws Exception {
         // given
-        Report report = new Report("김효중", "일했다");
+        ReportDTO reportDTO = new ReportDTO();
+        reportDTO.setAuthor("김효중");
+        reportDTO.setContent("일했다");
+
+        Report report = new Report(reportDTO);
         Long saveReportId = reportService.registerReport(report);
 
         // when
         LocalDate endDate = LocalDate.now().plusDays(1);
 
-        String startPeriod = LocalDateTime.now().getYear() + "-" +
-                             LocalDateTime.now().getMonthValue() + "-" +
-                             LocalDateTime.now().getDayOfMonth();
+        String startPeriod = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
 
-        String endPeriod = endDate.getYear() + "-" +
-                           endDate.getMonthValue() + "-" +
-                           endDate.getDayOfMonth();
+        String endPeriod = LocalDateTime.now().plusDays(1).format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
 
         List<Report> reports = reportService.searchReportsInPeriod(startPeriod, endPeriod);
 
         // then
         assertThat(reports.contains(report)).isEqualTo(true);
+    }
+
+    @Test
+    public void 업무일지_수정() throws Exception {
+        // given
+        ReportDTO reportDTO = new ReportDTO();
+        reportDTO.setAuthor("김효중");
+        reportDTO.setContent("일했다");
+
+        Long saveId = reportService.registerReport(new Report(reportDTO));
+        Report report1 = reportService.getReport(saveId);
+        em.flush();
+        em.clear();
+
+        ReportDTO reportDTO2 = new ReportDTO();
+        reportDTO2.setAuthor("김효일");
+        reportDTO.setContent("잘했다");
+
+        // when
+        Long modifyId = reportService.modifyReport(saveId, reportDTO2);
+        Report report2 = reportService.getReport(modifyId);
+
+        // then
+        assertThat(report1).isNotEqualTo(report2);
     }
 }
