0ba6e308dddd1d67a0ff70c0c50ef134810976ce
khj <zla1275@gamil.com>
2022-05-21 01:21:15 +0900

    Member.java
    사용자 엔티티 생성자 추가

--- src/main/java/com/mycompany/dailyreport/domain/Member.java
+++ src/main/java/com/mycompany/dailyreport/domain/Member.java
@@ -1,5 +1,6 @@
 package com.mycompany.dailyreport.domain;
 
+import com.mycompany.dailyreport.domain.dto.MemberDTO;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,16 +8,23 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Member extends CommonField{
+public class Member extends CommonField {
 
     @Id
     @GeneratedValue
     private Long id;
     private String name;
+
+    public Member(MemberDTO memberDTO) {
+        this.name = memberDTO.getName();
+        super.inputDate = LocalDateTime.now();
+        super.updateDate = LocalDateTime.now();
+    }
 }
