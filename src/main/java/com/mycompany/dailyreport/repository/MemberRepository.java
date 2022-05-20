6c0196db83f4a073048f2cb1bf1e38edc719cd8a
khj <zla1275@gamil.com>
2022-05-21 01:03:42 +0900

    MemberRepository.java 추가
    사용자 정보 저장 및 단건 조회 메서드 추가

---
+++ src/main/java/com/mycompany/dailyreport/repository/MemberRepository.java
@@ -0,0 +1,22 @@
+package com.mycompany.dailyreport.repository;
+
+import com.mycompany.dailyreport.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberRepository {
+
+    private final EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+}
