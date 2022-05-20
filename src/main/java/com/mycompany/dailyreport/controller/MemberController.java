320a37b4420ebb9390f02d4a299ad81302ab5db9
khj <zla1275@gamil.com>
2022-05-21 01:23:30 +0900

    MemberController.java 추가
    사용자 등록 메서드, 사용자 단건 조회 메서드 추가

---
+++ src/main/java/com/mycompany/dailyreport/controller/MemberController.java
@@ -0,0 +1,30 @@
+package com.mycompany.dailyreport.controller;
+
+import com.mycompany.dailyreport.domain.Member;
+import com.mycompany.dailyreport.domain.dto.MemberDTO;
+import com.mycompany.dailyreport.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping("member")
+    @ResponseBody
+    public String registerMember(@RequestBody MemberDTO memberDTO) {
+
+        memberService.registerMember(new Member(memberDTO));
+
+        return "OK";
+    }
+
+    @GetMapping("member/{id}")
+    @ResponseBody
+    public Member getMember(@PathVariable("id") Long id) {
+        return memberService.getMember(id);
+    }
+}
