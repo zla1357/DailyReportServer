5c523899e4988d899a3901020cf2076e0f529d7e
khj <zla1275@gamil.com>
2022-05-23 00:28:39 +0900

    MenuItemDTO
    생성자 추가'

--- src/main/java/com/mycompany/dailyreport/domain/dto/MenuItemDTO.java
+++ src/main/java/com/mycompany/dailyreport/domain/dto/MenuItemDTO.java
@@ -2,11 +2,15 @@ package com.mycompany.dailyreport.domain.dto;
 
 import com.mycompany.dailyreport.domain.ItemClass;
 import com.mycompany.dailyreport.domain.MenuItem;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class MenuItemDTO {
 
     private String menuName;
