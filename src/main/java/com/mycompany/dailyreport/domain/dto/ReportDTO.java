39f1b3b6cf7fd48c8c7658dc3082c2be73dab6c2
khj <zla1275@gamil.com>
2022-05-23 01:44:34 +0900

    ReportDTO.java
    속성에 MemberDTO 추가
    속성에서 memberId 제거
    
    Report 엔티티를 받아서 ReportDTO 로 변경해서 반환하는 메서드 추가

--- src/main/java/com/mycompany/dailyreport/domain/dto/ReportDTO.java
+++ src/main/java/com/mycompany/dailyreport/domain/dto/ReportDTO.java
@@ -1,18 +1,23 @@
 package com.mycompany.dailyreport.domain.dto;
 
+import com.mycompany.dailyreport.domain.ModelMapperUtils;
+import com.mycompany.dailyreport.domain.Report;
 import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
 public class ReportDTO {
 
     private Long id;
-    private Long memberId;
+    private MemberDTO member;
     private String content;
     private LocalDateTime inputDate;
     private LocalDateTime updateDate;
+
+    public static ReportDTO from(Report report) {
+        return ModelMapperUtils.getModelMapper().map(report, ReportDTO.class);
+    }
 }
