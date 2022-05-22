772bcb15234ba2bba63865931654be80f9a4fedf
khj <zla1275@gamil.com>
2022-05-22 23:34:04 +0900

    ReportDTO.java
    inputDate, updateDate 추가

--- src/main/java/com/mycompany/dailyreport/domain/dto/ReportDTO.java
+++ src/main/java/com/mycompany/dailyreport/domain/dto/ReportDTO.java
@@ -3,14 +3,15 @@ package com.mycompany.dailyreport.domain.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 public class ReportDTO {
 
+    private Long id;
     private Long memberId;
     private String content;
-    private LocalDate inputDate;
-    private LocalDate updateDate;
+    private LocalDateTime inputDate;
+    private LocalDateTime updateDate;
 }
