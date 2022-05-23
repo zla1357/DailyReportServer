7ce2760dc5819d2a7c7200bd5ec3075c00fa275c
khj <zla1275@gamil.com>
2022-05-24 00:48:56 +0900

    MenuItemService.java
    addMenuItem: 조회순번을 생성하는 구문 추가
    
    getMenuItem 추가

--- src/main/java/com/mycompany/dailyreport/service/MenuItemService.java
+++ src/main/java/com/mycompany/dailyreport/service/MenuItemService.java
@@ -1,6 +1,8 @@
 package com.mycompany.dailyreport.service;
 
+import com.mycompany.dailyreport.domain.ItemClass;
 import com.mycompany.dailyreport.domain.MenuItem;
+import com.mycompany.dailyreport.domain.dto.MenuItemDTO;
 import com.mycompany.dailyreport.repository.MenuItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,9 +15,36 @@ public class MenuItemService {
 
     private final MenuItemRepository menuItemRepository;
 
-    public Long addMenuItem(MenuItem menuItem) {
+    public Long addMenuItem(MenuItemDTO menuItemDTO) {
+
+        long sortSeq = getSortSeq(menuItemDTO);
+
+        MenuItem menuItem = new MenuItem(
+                menuItemDTO.getMenuName(),
+                sortSeq,
+                menuItemDTO.getItemClass(),
+                menuItemDTO.getParent());
 
         menuItemRepository.save(menuItem);
         return menuItem.getId();
+    }
+
+    private Long getSortSeq(MenuItemDTO menuItemDTO) {
+        long result;
+        if(isMasterItem(menuItemDTO)) {
+            result = menuItemRepository.getMasterMaxSortSeq() + 1;
+        } else {
+            result = menuItemDTO.getParent().getChild().size() + 1L;
+        }
+
+        return result;
+    }
+
+    private boolean isMasterItem(MenuItemDTO menuItemDTO) {
+        return menuItemDTO.getItemClass().equals(ItemClass.MASTER);
+    }
+
+    public MenuItem getMenuItem(Long id) {
+        return menuItemRepository.findOne(id);
     }
 }
