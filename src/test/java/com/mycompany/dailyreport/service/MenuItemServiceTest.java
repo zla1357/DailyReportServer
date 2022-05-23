920533850f19014f20f3a7f6f819d43289d5a417
khj <zla1275@gamil.com>
2022-05-24 00:07:06 +0900

    MenuItemServiceTest.java 추가

---
+++ src/test/java/com/mycompany/dailyreport/service/MenuItemServiceTest.java
@@ -0,0 +1,25 @@
+package com.mycompany.dailyreport.service;
+
+import com.mycompany.dailyreport.domain.MenuItem;
+import com.mycompany.dailyreport.domain.dto.MenuItemDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MenuItemServiceTest {
+
+    @Autowired
+    private MenuItemService menuItemService;
+
+    @Test
+    public void 메뉴항목_저장() throws Exception {
+        // given
+
+        // when
+
+        // then
+    }
+}
