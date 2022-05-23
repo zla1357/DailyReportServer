148ac61d3a7dfa249bb075a3574fbf83778e646e
khj <zla1275@gamil.com>
2022-05-23 22:18:58 +0900

    DailyReportApplication.java 이름 변경

--- src/main/java/com/mycompany/dailyreport/DailyReportApplication.java
+++ src/main/java/com/mycompany/dailyreport/DailyReportApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DailyreportApplication {
+public class DailyReportApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DailyreportApplication.class, args);
+		SpringApplication.run(DailyReportApplication.class, args);
 	}
 
 }
