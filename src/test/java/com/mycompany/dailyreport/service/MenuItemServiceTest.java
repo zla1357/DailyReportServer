a60b8fee3e72a89eca1df29d3ac156dac3edffc7
khj <zla1275@gamil.com>
2022-05-24 00:49:21 +0900

    MenuItemServiceTest.java
    메뉴항목 저장 메서드 테스트

--- src/test/java/com/mycompany/dailyreport/service/MenuItemServiceTest.java
+++ src/test/java/com/mycompany/dailyreport/service/MenuItemServiceTest.java
@@ -1,14 +1,17 @@
 package com.mycompany.dailyreport.service;
 
+import com.mycompany.dailyreport.domain.ItemClass;
 import com.mycompany.dailyreport.domain.MenuItem;
 import com.mycompany.dailyreport.domain.dto.MenuItemDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Transactional
 class MenuItemServiceTest {
 
     @Autowired
@@ -17,9 +20,19 @@ class MenuItemServiceTest {
     @Test
     public void 메뉴항목_저장() throws Exception {
         // given
+        MenuItemDTO menuItemDTO = new MenuItemDTO();
+        menuItemDTO.setMenuName("메뉴1");
+        menuItemDTO.setItemClass(ItemClass.MASTER);
 
         // when
+        Long savedId = menuItemService.addMenuItem(menuItemDTO);
+        System.out.println("savedId = " + savedId);
+        MenuItem menuItem = menuItemService.getMenuItem(savedId);
 
         // then
+        assertThat(menuItem.getId()).isEqualTo(1L);
+        assertThat(menuItem.getItemClass()).isEqualTo(ItemClass.MASTER);
+        assertThat(menuItem.getMenuName()).isEqualTo("메뉴1");
+        assertThat(menuItem.getSortSeq()).isEqualTo(1L);
     }
 }
