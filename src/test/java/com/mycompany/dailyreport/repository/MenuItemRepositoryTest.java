9358182848adf0533564a24e72123b0e768751f1
khj <zla1275@gamil.com>
2022-05-25 01:03:27 +0900

    MenuItemRepositoryTest.java
    메뉴아이템 등록 테스트에서 MenuItem 생성자에 DTO를 인자로 전달하도록 수정
    
    순번 max 조회 테스트에서 MenuItem 생성자에 DTO를 인자로 전달하도록 수정
    
    마스터메뉴 리스트 조회 테스트 추가

--- src/test/java/com/mycompany/dailyreport/repository/MenuItemRepositoryTest.java
+++ src/test/java/com/mycompany/dailyreport/repository/MenuItemRepositoryTest.java
@@ -2,14 +2,16 @@ package com.mycompany.dailyreport.repository;
 
 import com.mycompany.dailyreport.domain.ItemClass;
 import com.mycompany.dailyreport.domain.MenuItem;
-import org.assertj.core.api.Assertions;
+import com.mycompany.dailyreport.domain.dto.MenuItemDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -21,7 +23,7 @@ class MenuItemRepositoryTest {
     @Test
     public void 메뉴아이템_등록() throws Exception {
         // given
-        MenuItem menuItem = new MenuItem("업무일지", 1L, ItemClass.MASTER, null);
+        MenuItem menuItem = new MenuItem(new MenuItemDTO("업무일지", 1L, null, ItemClass.MASTER));
         
         // when
         menuItemRepository.save(menuItem);
@@ -30,13 +32,15 @@ class MenuItemRepositoryTest {
         // then
         assertThat(savedItem.getMenuName()).isEqualTo("업무일지");
         assertThat(savedItem.getItemClass()).isEqualTo(ItemClass.MASTER);
+        assertThat(savedItem.getSortSeq()).isEqualTo(1L);
     }
     
     @Test
     public void 순번_MAX_조회() throws Exception {
-        MenuItem menuItem = new MenuItem("업무일지", 1L, ItemClass.MASTER, null);
-        MenuItem menuItem2 = new MenuItem("업무일지2", 2L, ItemClass.MASTER, null);
-        MenuItem menuItem3 = new MenuItem("업무일지3", 3L, ItemClass.MASTER, null);
+
+        MenuItem menuItem = new MenuItem(new MenuItemDTO("업무일지", 1L, null, ItemClass.MASTER));
+        MenuItem menuItem2 = new MenuItem(new MenuItemDTO("업무일지2", 2L, null, ItemClass.MASTER));
+        MenuItem menuItem3 = new MenuItem(new MenuItemDTO("업무일지3", 3L, null, ItemClass.MASTER));
 
         // when
         menuItemRepository.save(menuItem);
@@ -47,5 +51,27 @@ class MenuItemRepositoryTest {
         System.out.println("maxSortSeq = " + maxSortSeq);
         // then
         assertThat(maxSortSeq).isEqualTo(3L);
+    }
+
+    @Test
+    public void Master_메뉴리스트_조회() throws Exception {
+        // given
+        MenuItem menuItem = new MenuItem(new MenuItemDTO("업무일지", 1L, null, ItemClass.MASTER));
+        MenuItem menuItem2 = new MenuItem(new MenuItemDTO("업무일지2", 2L, null, ItemClass.MASTER));
+        MenuItem menuItem3 = new MenuItem(new MenuItemDTO("업무일지3", 3L, null, ItemClass.MASTER));
+
+        menuItemRepository.save(menuItem);
+        menuItemRepository.save(menuItem2);
+        menuItemRepository.save(menuItem3);
+        List<MenuItem> items = new ArrayList<MenuItem>();
+        items.add(menuItem);
+        items.add(menuItem2);
+        items.add(menuItem3);
+
+        // when
+        List<MenuItem> masterMenuList = menuItemRepository.getMasterMenuList();
+
+        // then
+        assertThat(items).isEqualTo(masterMenuList);
     }
 }
