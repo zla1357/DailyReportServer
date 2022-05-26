4f4917ca1bec44768b5e45173b8a5ce46701e9a0
khj <zla1275@gamil.com>
2022-05-26 21:40:43 +0900

    DailyReportApplication.java
    스페이스 변경

--- src/main/java/com/mycompany/dailyreport/DailyReportApplication.java
+++ src/main/java/com/mycompany/dailyreport/DailyReportApplication.java
@@ -9,17 +9,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 public class DailyReportApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DailyReportApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DailyReportApplication.class, args);
+    }
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**");
-			}
-		};
-	}
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
 }
