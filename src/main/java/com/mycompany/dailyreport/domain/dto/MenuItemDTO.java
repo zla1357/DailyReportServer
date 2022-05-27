37c0cfab174bd7a3ea826762eed97880d02d535b
khj <zla1275@gamil.com>
2022-05-28 04:09:16 +0900

    MenuItemDTO.java
    id 추가

--- src/main/java/com/mycompany/dailyreport/domain/dto/MenuItemDTO.java
+++ src/main/java/com/mycompany/dailyreport/domain/dto/MenuItemDTO.java
@@ -14,6 +14,7 @@ import lombok.Setter;
 @AllArgsConstructor
 public class MenuItemDTO {
 
+    private Long id;
     private String menuName;
     private Long sortSeq;
     private MenuItem parent;
