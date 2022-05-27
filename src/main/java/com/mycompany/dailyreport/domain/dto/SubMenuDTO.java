c1689009ada7a75016cbc3694995b35e6680fb41
khj <zla1275@gamil.com>
2022-05-28 04:11:55 +0900

    SubMenuDTO.java 추가
    서브메뉴용 dto

---
+++ src/main/java/com/mycompany/dailyreport/domain/dto/SubMenuDTO.java
@@ -0,0 +1,25 @@
+package com.mycompany.dailyreport.domain.dto;
+
+import com.mycompany.dailyreport.domain.ItemClass;
+import com.mycompany.dailyreport.domain.MenuItem;
+import com.mycompany.dailyreport.domain.ModelMapperUtils;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SubMenuDTO {
+
+    private Long id;
+    private String menuName;
+    private Long sortSeq;
+    private ItemClass itemClass;
+
+    public static SubMenuDTO from(MenuItem menuItem) {
+        return ModelMapperUtils.getModelMapper().map(menuItem, SubMenuDTO.class);
+    }
+}
