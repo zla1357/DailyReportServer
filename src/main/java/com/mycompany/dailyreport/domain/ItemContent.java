3eaac8c55c9d480ec8dad0d05130ca75f6939a02
khj <zla1275@gamil.com>
2022-05-30 00:09:46 +0900

    ItemContent.java
    아이템 내용 추가

---
+++ src/main/java/com/mycompany/dailyreport/domain/ItemContent.java
@@ -0,0 +1,18 @@
+package com.mycompany.dailyreport.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ItemContent {
+
+    private String reqUrl;
+
+    // 콤마로 구분된 파라미터 리스트
+    private String reqParam;
+}
