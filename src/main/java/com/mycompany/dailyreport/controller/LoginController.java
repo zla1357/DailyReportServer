88ab112736501420ff688d7ffb174a0ffed9f781
khj <zla1275@gamil.com>
2022-06-06 01:34:49 +0900

    로그인 컨트롤러 추가

---
+++ src/main/java/com/mycompany/dailyreport/controller/LoginController.java
@@ -0,0 +1,9 @@
+package com.mycompany.dailyreport.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class LoginController {
+
+}
