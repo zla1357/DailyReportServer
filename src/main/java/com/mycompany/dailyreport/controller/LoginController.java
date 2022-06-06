0fb672f477b9b4afa022ddc602db040de775bef1
khj <zla1275@gamil.com>
2022-06-06 22:53:05 +0900

    LoginController.java추가: 로그인 메서드 추가 LoginDTO.java추가: 로그인 정보를 받기 위한 DTO 추가

--- src/main/java/com/mycompany/dailyreport/controller/LoginController.java
+++ src/main/java/com/mycompany/dailyreport/controller/LoginController.java
@@ -1,9 +1,29 @@
 package com.mycompany.dailyreport.controller;
 
+import com.mycompany.dailyreport.domain.Member;
+import com.mycompany.dailyreport.domain.dto.LoginDTO;
+import com.mycompany.dailyreport.domain.dto.MemberDTO;
+import com.mycompany.dailyreport.service.LoginService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequiredArgsConstructor
 public class LoginController {
 
+    private final LoginService loginService;
+
+    @PostMapping("/login")
+    @ResponseBody
+    public MemberDTO login(@RequestBody LoginDTO loginDTO) {
+        MemberDTO loginMemberDTO = new MemberDTO();
+        loginMemberDTO.setAccountId(loginDTO.getAccountId());
+        loginMemberDTO.setPassword(loginDTO.getPassword());
+
+        Member loginMember = loginService.login(new Member(loginMemberDTO));
+        return MemberDTO.from(loginMember);
+    }
 }
---
+++ src/main/java/com/mycompany/dailyreport/domain/dto/LoginDTO.java
@@ -0,0 +1,10 @@
+package com.mycompany.dailyreport.domain.dto;
+
+import lombok.Getter;
+
+@Getter
+public class LoginDTO {
+
+    private String accountId;
+    private String password;
+}
